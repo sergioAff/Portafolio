caf156a248c2fb966807fca995c6f55682d3eada
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2024-10-30 13:06:11 -0300

    order experience

--- src/data/experiencia.ts
+++ src/data/experiencia.ts
@@ -4,21 +4,39 @@ export const Experiencia = () => {
   const { t } = useTranslation(["experiencias"]);
   return [
     {
-      trabajo: t("experiencias.7.title"),
-      descripcion: t("experiencias.7.description"),
-      lugar: t("experiencias.7.place"),
+      trabajo: t("experiencias.5.title"),
+      descripcion: t("experiencias.5.description"),
+      lugar: t("experiencias.5.place"),
       fechas: {
-        inicio: new Date(2020, 5),
-        fin: new Date(2023, 2),
+        inicio: new Date(2024, 5),
+        fin: new Date(2024, 9),
       },
     },
     {
-      trabajo: t("experiencias.2.title"),
-      descripcion: t("experiencias.2.description"),
-      lugar: t("experiencias.2.place"),
+      trabajo: t("experiencias.6.title"),
+      descripcion: t("experiencias.6.description"),
+      lugar: t("experiencias.6.place"),
       fechas: {
-        inicio: new Date(2022, 10),
-        fin: new Date(2023, 2),
+        inicio: new Date(2024, 1),
+        fin: new Date(2024, 4),
+      },
+    },
+    {
+      trabajo: t("experiencias.4.title"),
+      descripcion: t("experiencias.4.description"),
+      lugar: t("experiencias.4.place"),
+      fechas: {
+        inicio: new Date(2024, 1),
+        fin: new Date(2024, 3),
+      },
+    },
+    {
+      trabajo: t("experiencias.1.title"),
+      descripcion: t("experiencias.1.description"),
+      lugar: t("experiencias.1.place"),
+      fechas: {
+        inicio: new Date(2023, 8),
+        fin: new Date(2024, 1),
       },
     },
     {
@@ -32,40 +50,21 @@ export const Experiencia = () => {
       },
     },
     {
-      trabajo: t("experiencias.1.title"),
-      descripcion: t("experiencias.1.description"),
-      lugar: t("experiencias.1.place"),
+      trabajo: t("experiencias.2.title"),
+      descripcion: t("experiencias.2.description"),
+      lugar: t("experiencias.2.place"),
       fechas: {
-        inicio: new Date(2023, 8),
-        fin: new Date(2024, 1),
-      },
-    },
-
-    {
-      trabajo: t("experiencias.4.title"),
-      descripcion: t("experiencias.4.description"),
-      lugar: t("experiencias.4.place"),
-      fechas: {
-        inicio: new Date(2024, 1),
-        fin: new Date(2024, 3),
+        inicio: new Date(2022, 10),
+        fin: new Date(2023, 2),
       },
     },
     {
-      trabajo: t("experiencias.6.title"),
-      descripcion: t("experiencias.6.description"),
-      lugar: t("experiencias.6.place"),
+      trabajo: t("experiencias.7.title"),
+      descripcion: t("experiencias.7.description"),
+      lugar: t("experiencias.7.place"),
       fechas: {
-        inicio: new Date(2024, 1),
-        fin: new Date(2024, 4),
-      },
-    },
-    {
-      trabajo: t("experiencias.5.title"),
-      descripcion: t("experiencias.5.description"),
-      lugar: t("experiencias.5.place"),
-      fechas: {
-        inicio: new Date(2024, 5),
-        fin: new Date(),
+        inicio: new Date(2020, 5),
+        fin: new Date(2023, 2),
       },
     },
   ];
