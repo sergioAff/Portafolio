28f3b3cea52c76c57d1bebc5e87d8bc227538265
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2024-11-02 21:46:52 -0300

    fix olas

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,7 @@
 import { useTranslation } from "react-i18next";
 import { Links } from "./Links";
 import { contactos } from "@/data/contactos";
+import { Olas } from "@/components/Olas";
 
 export function Footer() {
   const { t } = useTranslation(["footer"]);
@@ -43,80 +44,7 @@ export function Footer() {
         </h3>
       </div>
       <div className="animate-fadeBottom absolute w-full h-screen bottom-0 pointer-events-auto z-10 ">
-        <svg
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          width="100%"
-          height="100%"
-          viewBox="0 0 1600 900"
-          preserveAspectRatio="none"
-        >
-          <defs>
-            <linearGradient id="bg">
-              <stop
-                offset="0%"
-                // style={{ stopColor: "rgba(249, 115, 22,0.2)" }}
-                style={{ stopColor: "rgba(245, 119, 36,0.45)" }}
-              ></stop>
-              <stop
-                offset="100%"
-                // style={{ stopColor: "rgba(249, 105, 80,0.2)" }}
-                style={{ stopColor: "rgba(249, 110, 45,0.3)" }}
-              ></stop>
-            </linearGradient>
-            <path
-              id="wave"
-              fill="url(#bg)"
-              d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
-                c268.487,41.997,472.477,5.293,715.971,0c243.494-5.293,505.474,5.859,505.474,48.368
-                c0,42.509,252.489,71.963,505.963,66.963v560.106H-363.852V502.589z"
-            />
-          </defs>
-          <g>
-            <use xlinkHref="#wave" opacity=".3">
-              <animateTransform
-                dur="10s"
-                attributeName="transform"
-                attributeType="XML"
-                type="translate"
-                calcMode="spline"
-                values="270 230; -334 180; 270 230"
-                keyTimes="0; 0.5; 1"
-                keySplines="0.42, 0, 0.58, 1.0; 0.42, 0, 0.58, 1.0"
-                repeatCount="indefinite"
-              />
-            </use>
-            <use xlinkHref="#wave" opacity=".6">
-              <animateTransform
-                dur="8s"
-                attributeName="transform"
-                attributeType="XML"
-                type="translate"
-                calcMode="spline"
-                values="270 230; -334 180; 270 230"
-                keyTimes="0; 0.5; 1"
-                keySplines="0.42, 0, 0.58, 1.0; 0.42, 0, 0.58, 1.0"
-                repeatCount="indefinite"
-              />
-            </use>
-            <use xlinkHref="#wave" opacity=".9">
-              <animateTransform
-                dur="6s"
-                attributeName="transform"
-                attributeType="XML"
-                type="translate"
-                calcMode="spline"
-                values="270 230; -334 180; 270 230"
-                keyTimes="0; 0.5; 1"
-                keySplines="0.42, 0, 0.58, 1.0; 0.42, 0, 0.58, 1.0"
-                repeatCount="indefinite"
-              />
-            </use>
-          </g>
-        </svg>
+        <Olas />
       </div>
     </footer>
   );
