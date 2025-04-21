73049a04fafb30c5b3a3c2ac0374c10d07da2f36
Sergio Fernandez <sergioadrianfernandez02@gmail.com>
2025-04-21 09:35:37 -0300

    feat: Add meta tags for SEO optimization in layout component
    This commit introduces several meta tags in the `layout.tsx` file to enhance SEO for Sergio Fernández's personal website. The added tags include description, keywords, author, and various directives for search engines, improving the site's visibility and indexing.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,22 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#F9EEE7" />
         <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+        <meta
+          name="description"
+          content="Sergio Fernández's personal website, where you can find information about his projects and contact him."
+        />
+        <meta
+          name="keywords"
+          content="Sergio Fernández, software engineer, developer, projects, contact, portfolio, about me, skills, experience, education, certifications, projects, contact, full stack, frontend, backend, javascript, typescript, react, node.js, python, php, mysql, postgresql, docker, kubernetes, aws, azure, devops, agile, scrum, kanban, project management, team leader, team management, leadership, communication, teamwork, problem solving, critical thinking, creativity, innovation, design, ui/ux, user experience, user interface, user testing, user research, user feedback, user testing, user research, user feedback, user testing, user research, user feedback, user testing, user research, user feedback"
+        />
+        <meta name="author" content="Sergio Fernández" />
+        <meta name="robots" content="index, follow" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="distribution" content="web" />
+        <meta name="rating" content="general" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="google" content="notranslate" />
+        <meta name="google-site-verification" content="google-site-verification=google-site-verification" />    
         <title>S@ff</title>
       </head>
       <body
